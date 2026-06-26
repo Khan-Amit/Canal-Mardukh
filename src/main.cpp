e28a1f66dafe92facfc1d5eb5a4779eb41a75820
ENKI <amit.khanna.1082@gmail.com>
2026-06-26 14:27:31 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+int main()
+{
+    std::cout << "=========================================\n";
+    std::cout << "        Canal-Mardukh (TM)\n";
+    std::cout << "     Part of Mardukh System (TM)\n";
+    std::cout << "=========================================\n\n";
+
+    std::cout << "Status : Initializing...\n";
+    std::cout << "Version: 0.1.0\n\n";
+
+    std::cout << "Wallet Address:\n";
+    std::cout << "45ktWDeTNtUcVMXfJRKS6bbXMznMAStZFX6niJHcVy9uQk132bHJ21QTC5AKvqyx9XJN5e7mPc3vViyGnB2BM6DD1ZoAoZb\n\n";
+
+    std::cout << "Launching Core...\n";
+
+    return 0;
+}
