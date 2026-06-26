b0e394bbe3853794c82d04d914bbaa658d222407
ENKI <amit.khanna.1082@gmail.com>
2026-06-26 14:33:22 +0700

    Refactor main.cpp to use Application class

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
-#include <iostream>
+#include "Application.h"
 
 int main()
 {
-    std::cout << "=========================================\n";
-    std::cout << "        Canal-Mardukh (TM)\n";
-    std::cout << "     Part of Mardukh System (TM)\n";
-    std::cout << "=========================================\n\n";
+    Application app;
 
-    std::cout << "Status : Initializing...\n";
-    std::cout << "Version: 0.1.0\n\n";
+    if (!app.Initialize())
+        return -1;
 
-    std::cout << "Wallet Address:\n";
-    std::cout << "45ktWDeTNtUcVMXfJRKS6bbXMznMAStZFX6niJHcVy9uQk132bHJ21QTC5AKvqyx9XJN5e7mPc3vViyGnB2BM6DD1ZoAoZb\n\n";
+    app.Run();
 
-    std::cout << "Launching Core...\n";
+    app.Shutdown();
 
     return 0;
 }
